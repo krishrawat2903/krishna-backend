c7f9191d9261ad821849582f9b5c514dec9e2e2c
krishrawat2903 <krishrawat2903@gmail.com>
2026-03-22 13:52:27 +0530

    Update SMTP config for Render

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ app.use(cors())
 app.use(express.json())
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 })
-
 app.get("/", (req, res) => {
   res.send("Backend is running")
 })
